89b26b04fe9d40f0193ba075643794ef92c570d0
Kavu12 <kavusikan22@gmail.com>
2025-02-13 16:42:27 +0100

    Question 4, Modification de la méthode getTemplate1 et ajout du model

--- src/main/java/fr/devavance/tp_springboot_mvc_jpa/controllers/Controller.java
+++ src/main/java/fr/devavance/tp_springboot_mvc_jpa/controllers/Controller.java
@@ -1,12 +1,15 @@
 package fr.devavance.tp_springboot_mvc_jpa.controllers;
 
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @org.springframework.stereotype.Controller
 public class Controller {
 
     @RequestMapping("/")
-    public String getTemplate1(){
+    public String getTemplate1(Model m){
+        m.addAttribute("name", "Capteur température");
         return "template1";
     }
 }
